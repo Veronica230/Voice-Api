022608504a017e889669935c6d1a125a7f0a1014
TadeoDaniel19 <hector_lectric@hotmail.com>
2021-06-10 00:03:49 -0500

    js: add preview files for inputs

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
+const changeExt = (fileName, newExt) => {
+  const pos = fileName.includes(".")
+    ? fileName.lastIndexOf(".")
+    : fileName.length;
+  const fileRoot = fileName.substr(0, pos);
+  const output = `${fileRoot}.${newExt}`;
+  return output;
+};
+
 const getTargetPath = (event) => {
-/*   const targetFile = document.getElementById("fileTarget").files[0];
-  const reader = new FileReader();
-    reader.readAsDataURL(targetFile);
-    reader.onloadend = () => {
-      sessionStorage.setItem("targetPath", reader.result);
-  }
-   */
   const value = event.target.value;
   sessionStorage.setItem("targetPath", value);
+  const changeFormat = changeExt(value, 'png')
+  document.getElementById("img-result").src = changeFormat;
 };
 
 const getModelPath = (event) => {
-/*   const modelFile = document.getElementById("fileModel").files[0];
-  const reader = new FileReader();
-  reader.readAsDataURL(modelFile);
-  reader.onloadend = () => {
-    sessionStorage.setItem("modelPath", reader.result);
-} */
-
-const value = event.target.value;
-sessionStorage.setItem("modelPath", value);
+  const value = event.target.value;
+  sessionStorage.setItem("modelPath", value);
+  const divModel = document.getElementById("render-model-img");
+  divModel.innerHTML = `<h1>Error: No se puede previsualizar un modelo tipo gtlf</h1>`;
 };
 
 const getStatus = () => {
-  sessionStorage.removeItem("targetPath")
-  sessionStorage.removeItem("modelPath")
+  sessionStorage.removeItem("targetPath");
+  sessionStorage.removeItem("modelPath");
   if (window.confirm("¿Deseas hacer resetear de los datos?")) {
     sessionStorage.removeItem("targetPath");
     sessionStorage.removeItem("modelPath");
   }
-}
+};
 
 const reloadVideoZone = () => {
   if (window.confirm("¿Deseas enviar los datos?")) {
-    //document.getElementById("video-zone").innerHTML.reload
     location.reload();
   }
-}
+};
